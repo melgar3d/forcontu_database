07f4243f5eea13d79d263c73e896d603459dc27d
albermelg <melgar3d.am@gmail.com>
2021-04-11 18:38:06 +0200

    Actividad 21.3a - Select

--- src/Controller/ForcontuDatabaseController.php
+++ src/Controller/ForcontuDatabaseController.php
@@ -16,11 +16,11 @@ use Drupal\dblog\Controller;
 class ForcontuDatabaseController extends ControllerBase{
 
     protected $database;
-    protected $currentUser;
+    protected $current_user;
 
     public function __construct(Connection $database, AccountInterface $current_user) {
         $this->database = $database;
-        $this->currentUser = $currentUser;
+        $this->current_user = $current_user;
     }
 
     public static function create(ContainerInterface $container){
@@ -34,17 +34,18 @@ class ForcontuDatabaseController extends ControllerBase{
         
         $query = $this->database->select('comment_field_data', 'n')
         ->fields('n', ['cid', 'entity_id', 'subject', 'uid']);
-        //$query->rightJoin('comment', 'n');
-        //dpq($query);
+        $query->innerJoin('comment', 'c', 'c.cid = n.uid');
+        dpq($query);
         $result = $query->execute();
 
+        $header = ['cid', 'entity_id', 'subject', 'uid'];
         $rows = $result->fetchAllAssoc('cid', \PDO::FETCH_ASSOC);
 
         dpm($rows);
 
         $build['forcontu_comment_table'] = [
             '#type' => 'table',
-            //'#header' => $header,
+            '#header' => $header,
             '#rows' => $rows,
         ];
 
